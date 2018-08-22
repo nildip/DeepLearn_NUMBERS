e0c718baf72772f652620928d76d31b1e685936a
Nildip Mukherjee <mukherjee.nildip@gmail.com>
2018-08-22 23:11:41 +0530

    Add files via upload

--- utils.py
+++ utils.py
@@ -1,8 +1,11 @@
 import matplotlib.pyplot as plt
+import sys
 import itertools
 import numpy as np
 from sklearn.metrics import confusion_matrix
+from keras import backend as K
 
+# function to plot the confusion matrix
 def plot_confusion_matrix(Y_true, Y_predicted, classes, normalize=False):
     cm = confusion_matrix(Y_true, Y_predicted, labels = classes)
     plt.figure()
@@ -21,3 +24,19 @@ def plot_confusion_matrix(Y_true, Y_predicted, classes, normalize=False):
     plt.ylabel('True label')
     plt.xlabel('Predicted label')
     plt.show()
+    
+# function to visualize 2D CNN filters    
+def visualize_filter(model, input_X, input_Y, layer_num):
+    if type(model.layers[layer_num]) != keras.layers.convolutional.Conv2D:
+        sys.exit('The selected layer is not a 2D CNN filter') 
+    get_filter_output = K.function([model.layers[0].input], [model.layers[layer_num].output])
+    for k in range(0,10):
+        X_layer = np.mean(input_X[np.where(input_Y == k)], axis=0)
+        X_layer = np.expand_dims(X_layer, axis=0)
+        X_layer = np.expand_dims(X_layer, axis=4)
+        layer_output = get_filter_output([X_layer])[0]
+        plt.figure(figsize=(20,20))
+        for i in range(0, model.layers[k].get_config()['filters']):
+            plt.subplot(1, 5, i+1)
+            plt.title('Output of filter {0}, input = {1}'.format(i+1, k))
+            plt.imshow(layer_output[0][:,:,i], cmap = plt.get_cmap('gray_r'))
